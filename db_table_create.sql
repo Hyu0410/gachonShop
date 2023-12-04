091be8bb0c35bfe671695279c63813d2f37c6277
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2023-12-04 17:28:56 +0900

    merchandise CUD
    상품 생성, 수정, 삭제(관리자용)

---
+++ db_table_create.sql
@@ -0,0 +1,35 @@
+create table person (
+loginid varchar(10) not null,
+password varchar(10) not null,
+name varchar(20) not null,
+address varchar(50) null,
+tel varchar(13) not null, 
+birth varchar(8) not null, 
+class varchar(2) not null, /*00 : CEO, 01 : 관리자, 02 : 일반고객 */
+point int, 
+PRIMARY KEY(loginid)
+);
+
+create table code_tbl (
+main_id varchar(4) not null,
+sub_id varchar(4) not null,
+main_name varchar(100) not null,
+sub_name varchar(100) not null,
+start varchar(8) not null,
+end varchar(8) not null,
+PRIMARY KEY(main_id,sub_id)
+);
+
+create table merchandise (
+mer_id int NOT NULL auto_increment,
+category varchar(4) not null,
+name varchar(100) not null,
+price int not null,
+stock int not null, 
+brand varchar(100) not null, 
+supplier varchar(100) not null, 
+image varchar(50), 
+sale_yn varchar(1),
+sale_price int,
+PRIMARY KEY(mer_id)
+);
--- main.js
+++ main.js
@@ -5,6 +5,8 @@ app.set('view engine', 'ejs');
 
 var rootRouter = require('./router/rootRouter');
 var authRouter = require('./router/authRouter');
+var merchandiseRouter = require('./router/merchandiseRouter');
+var codeRouter = require('./router/codeRouter');
 
 // 세션 모듈
 var session = require('express-session');
@@ -28,7 +30,12 @@ app.use(session({
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
+// 정적 파일 폴더 지정
+app.use(express.static('public'));
+
 app.use('/', rootRouter);
 app.use('/auth', authRouter);
+app.use('/merchandise', merchandiseRouter);
+app.use('/code', codeRouter);
 
 app.listen(3000, () => console.log('3000!'));
